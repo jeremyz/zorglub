b3e28a42d3982c16f46ea9ec1a02d2f526e1510f
Jérémy Zurcher <jeremy@asynk.ch>
2012-01-04 02:50:32 +0100

    disable autoload for major classes

--- lib/zorglub.rb
+++ lib/zorglub.rb
@@ -1,14 +1,13 @@
 #! /usr/bin/env ruby
 # -*- coding: UTF-8 -*-
+require 'zorglub/config'
+require 'zorglub/node'
+require 'zorglub/app'
 #
 module Zorglub
     #
     VERSION = '0.0.1'
     #
-    autoload :App,      './lib/zorglub/app.rb'
-    autoload :Config,   './lib/zorglub/config.rb'
-    autoload :Helpers,  './lib/zorglub/helpers.rb'
-    autoload :Node,     './lib/zorglub/node.rb'
     autoload :Session,  './lib/zorglub/session.rb'
     #
 end
