b3cdcc68ca8a7831855d0c87f82006f73041c86f
Jérémy Zurcher <jeremy@asynk.ch>
2012-01-05 15:35:10 +0100

    add file engine

---
+++ lib/zorglub/engines/file.rb
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+#
+require 'haml/util'
+require 'haml/engine'
+#
+module Zorglub
+    module Engines
+        module File
+            def self.proc path,obj
+                content = ::File.open(path,'r'){|f| f.read }
+                ext = path.sub /.*\.(.+)$/,'\1'
+                return content, "text/#{ext}"
+            end
+        end
+    end
+end
+#
+Zorglub::Config.register_engine :file, nil, Zorglub::Engines::File.method(:proc)
+#
+# EOF
