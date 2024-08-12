bc1d6ba68b2ef61ff24b48a27cd0ce412d363d49
Jérémy Zurcher <jeremy@asynk.ch>
2024-08-12 13:38:19 +0200

    fix sass engine caching

--- lib/zorglub/engines/sass.rb
+++ lib/zorglub/engines/sass.rb
@@ -4,10 +4,11 @@ module Zorglub
   module Engines
     module Sass
       def self.proc(path, obj)
-        sass = ::Sass::Engine.new(::File.read(path), obj.app.opt(:sass_options))
         if obj.app.opt(:engines_cache_enabled)
           key = path.sub obj.app.opt(:root), ''
-          obj.app.engines_cache[key] ||= sass
+          sass = obj.app.engines_cache[key] || ::Sass::Engine.new(::File.read(path), obj.app.opt(:sass_options))
+        else
+          sass = ::Sass::Engine.new(::File.read(path), obj.app.opt(:sass_options))
         end
         css = sass.render
         [css, 'text/css']
