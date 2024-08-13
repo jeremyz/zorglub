c28a42e3475535f033f4d51fc8cd24febb28afd5
Jérémy Zurcher <jeremy@asynk.ch>
2024-08-13 11:50:16 +0200

    haml encoding option is deprecated

--- lib/zorglub/app.rb
+++ lib/zorglub/app.rb
@@ -18,7 +18,6 @@ module Zorglub
         engines: {},
         haml_options: {
           format: :html5,
-          encoding: 'utf-8',
           escape_html: false
         },
         sass_options: {
